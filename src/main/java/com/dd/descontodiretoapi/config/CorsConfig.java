89f4c75300c3093a0fe319211800bb6770eb386a
Yasmine Belmiro <devyasminebelmiro@gmail.com>
2025-11-05 19:27:27 -0300

    refactor: configurando o cors para conectar no versel

--- src/main/java/com/dd/descontodiretoapi/config/CorsConfig.java
+++ src/main/java/com/dd/descontodiretoapi/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://desconto-direto.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
