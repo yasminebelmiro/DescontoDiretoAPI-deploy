9b5d1911de61fc0543425aa414b5e8261edce53f
Yasmine Belmiro <devyasminebelmiro@gmail.com>
2025-11-18 08:45:49 -0300

    refactor: desbloqueando o cors

--- src/main/java/com/dd/descontodiretoapi/config/CorsConfig.java
+++ src/main/java/com/dd/descontodiretoapi/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://desconto-direto.vercel.app")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
