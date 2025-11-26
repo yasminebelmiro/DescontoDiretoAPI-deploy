2d71f7dd431c0832db3ee3497c005c0bc8f1c421
Yasmine Belmiro <devyasminebelmiro@gmail.com>
2025-11-26 07:49:03 -0300

    chore: configurando o cors para acessar o deploy

--- src/main/java/com/dd/descontodiretoapi/config/CorsConfig.java
+++ src/main/java/com/dd/descontodiretoapi/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("*")
+                        .allowedOrigins("https://descontodiretoapi.up.railway.app/", "http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
